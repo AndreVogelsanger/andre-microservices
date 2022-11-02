10e49488e61b4d245ca0e34a0369921a45a27a0d
Andre Vogelsanger <32198958+AndreVogelsanger@users.noreply.github.com>
2022-11-02 18:08:23 -0300

    add

--- greeting/src/main/java/br/com/andre/configuration/GreetingConfiguration.java
+++ greeting/src/main/java/br/com/andre/configuration/GreetingConfiguration.java
@@ -6,15 +6,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Named;
 
-
-@Component @ConfigurationProperties("greeting-service")
-@AllArgsConstructor @NoArgsConstructor
-@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Named
+@Component
+@RefreshScope
+@ConfigurationProperties("greeting-service")
 public class GreetingConfiguration {
 
     private String greeting;
